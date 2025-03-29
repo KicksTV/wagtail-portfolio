1a021ecb95b3c005d85f5c40b946f07ad9835790
Llewellyn <llewellynbh@gmail.com>
2025-03-29 19:16:15 +0000

    init

---
+++ KiX/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-!#d54s5xv^_)^44da@ys=th(f75eirxrf$&o_t$ud*t$a$+4fx"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
